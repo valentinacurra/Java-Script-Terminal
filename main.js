02cf83849ba3b034b94216c6fa2ef1b51bb3df97
Valentina Curra <146252235+valentinacurra@users.noreply.github.com>
2023-11-22 15:18:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,204 @@
+// // variáveis
+
+// // let variavel que pode mudar
+
+// let nome = "Valentina"
+
+// console.log(nome)
+
+// nome = "Valentina Curra"
+
+// console.log(nome)
+
+// // const variavel que não muda, constante
+
+// const sobrenome = "de Paula"
+
+// sobrenome = "Curra"
+
+// console.log(sobrenome)
+
+// // Não é para usar essa variavel
+
+// var nome2 = "Valentina"
+
+// querySelector para escolher oq eu quero mostrar
+
+// const myHeading = document.querySelector("h1")
+
+// // myHeading.textContent = "Hello world"
+
+// const meuBotao = document.getElementById("meuBotao")
+// meuBotao.addEventListener("click", function(){
+//     alert("O botão foi clicado!")
+// })
+
+// // function nome(nomeUm){
+// //     return ("Meu nome é" + nomeUm)
+// // console.log(nome("Valentina"))
+
+// let verdade = true;
+
+// let a = 10;
+
+// if(verdade){
+//     console.log("Dentro do IF: ", a);
+
+// }
+
+// let b = 30;
+// console.log("Fora do IF: ", b);
+
+// if(x > 0){
+//     console.log("Dentro do IF: ", a);
+
+
+// }
+
+// && = and 
+// || = or
+
+// Let A = 10
+// Let B = "10"
+
+// python:
+
+// A == B
+
+// java:
+// A === B
+
+// tres simbolos: 
+// >==
+// <==
+
+
+// const input = document.querySelector("#input")
+
+
+// const input = document.querySelector("#input");
+
+// const input1 = document.querySelector("#input1");
+
+// const button = document.querySelector("#botao");
+
+// const resultado = document.querySelector("#resultado");
+
+// botao.addEventListener("click", function (){
+//     let valor = 0
+//     valor = Number(input.value) + Number(input1.value);
+
+
+//     resultado.textContent = "Resultado: " + valor;
+// })
+
+
+// let nomePersonagem = "Valentina"
+
+// let sobrenome = "De Paula"
+
+// let idadePersonagem = "16"
+
+// let empregado = "sim."
+
+// // como usar console.log:
+
+// console.log(nomePersonagem)
+// console.log(sobrenome)
+// console.log(idadePersonagem)
+// console.log(empregado)
+
+// // melhores formas:
+
+// console.log(nomePersonagem + sobrenome + idadePersonagem + empregado)
+
+// console.log("Meu nome é", nomePersonagem, "e meu sobrenome é", sobrenome, "minha idade é", idadePersonagem, "anos. Estou empregado:", empregado)
+
+// console.log(`Meu nome é ${nomePersonagem} e meu sobrenome é ${sobrenome} minha idade é ${idadePersonagem} anos. Estou empregado ${empregado}`)
+
+// ------------------------------------------------------------------------------------------------------------
+
+// typeof diz o tipo da váriavel
+
+// const nomeUsuario = prompt("Digite seu nome")
+
+// let idade = Number(prompt("Digite sua idade"))
+// console.log(typeof idade)
+
+// idade.toString()
+// console.log(typeof idade)
+
+
+// console.log(typeof nomeUsuario)
+
+// var playBtn = document.getElementById('play'),
+//     resetBtn = document.getElementById('reset'),
+//     hearbeat = document.getElementById('heartbeat')
+// audios = document.querySelectorAll('audio');
+// console.log(audios);
+
+
+// playBtn.addEventListener('mouseover', function () {
+//     [].forEach.call(audios, function (audio) {
+//         // do whatever
+//         audio.play();
+//     });
+// }, false);
+
+// playBtn.addEventListener('mouseleave', function () {
+//     heartbeat.pause();
+//     heartbeat.currentTime = 0;
+// }, false);
+
+// resetBtn.addEventListener('mouseover', function () {
+//     heartbeat.play();
+// }, false);
+
+// resetBtn.addEventListener('mouseleave', function () {
+//     heartbeat.pause();
+//     heartbeat.currentTime = 0;
+// }, false);
+
+
+// function abrir(URL) {
+//     window.open(URL, 'janela', 'width=795, height=590, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+//   }
+
+// let nome
+
+// let cidade
+
+// console.log(typeof nome, typeof cidade)
+
+// nome = String(prompt("Digite eu nome:"))
+
+// cidade = String(prompt("Digite sua cidade:"))
+
+// console.log(typeof nome, typeof cidade)
+
+// console.log("Olá meu nome é ", nome, "e eu moro em ", cidade)
+
+// let pergunta1
+// let pergunta2
+// let pergunta3
+
+// pergunta1 = String(prompt("Você gosta de programar? (sim) (não)"))
+// pergunta2 = String(prompt("Você ja viajou para fora do país? (sim) (não)"))
+// pergunta3 = String(prompt("Você tem animais de estimação? (sim) (não)"))
+
+
+// console.log("Você gosta de programar?", pergunta1, "Você ja viajou para fora do país?", pergunta2, "Você tem animais de estimação?", pergunta3)
+
+
+// let numero1
+// let numero2
+ 
+// numero1 = Number(prompt("Insira seu primeiro valor"))
+// numero2 = Number(prompt("Insira seu segundo valor"))
+ 
+// let X = numero1 + numero2
+// let Y = numero1 * numero2
+ 
+// console.log("A soma dos números é:", X)
+// console.log("A multiplicação dos números é:", Y) 
+
